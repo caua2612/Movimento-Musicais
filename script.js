eadd509d2db6f83f13c1ccd3e4f3c9ec86e4b67a
caua2612 <167487865+caua2612@users.noreply.github.com>
2025-03-31 22:53:00 +0000

    31/03

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function (){
+  let tamanhoAtualFonte =1;
+
+  const botaoAumentarFonte = document.getElementById('aumentar-fonte');
+  const botaoDiminuirFonte = document.getElementById('diminuir-fonte');
+
+  botaoAumentarFonte.addEventListener('click', function(){
+   tamanhoAtualFonte += 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+});
